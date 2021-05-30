bcc1390534c3614bae054989c6aa0a6309ae88f0
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-05-30 21:11:32 +0300

    Create apiService.js

---
+++ src/js/apiService.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export default class ApiService {
+  #API_KEY = '21149088-9f5924478b20a01769fc809e0';
+  
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 16;
+  }
+  
+  fetchPhotos() {
+    const url = new URLSearchParams({
+      image_type: 'photo',
+      orientation: 'horizontal',
+      q: this.searchQuery,
+      page: this.page,
+      per_page: this.perPage,
+      key: this.#API_KEY,
+    });
+
+    return axios.get(`?${url}`)
+      .then(response => response.data.hits)
+      .then(this.incrementPage());
+  };
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}

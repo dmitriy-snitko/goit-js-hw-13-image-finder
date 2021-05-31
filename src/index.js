66664696f67ee65a5b12b42e529d6573f5c3fa46
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-01 01:21:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import './sass/main.scss';
+import '../node_modules/lightbox2/dist/css/lightbox.css';
 
 import debounce from 'lodash.debounce';
 import ApiService from './js/apiService.js';
 import refs from './js/refs.js';
 import { observer } from './js/intersectionObserver.js';
-import { renderCardsMarckup, resetCardsMarckup } from './js/renderMarckup';
+import { renderCardsMarckup } from './js/renderMarckup';
+import { notifications, onError } from './js/notifications';
+import lightbox from 'lightbox2';
+
+
+lightbox.option({
+  disableScrolling: true,
+});
 
 export const photosApiService = new ApiService();
 
 refs.form.addEventListener('input', debounce(onSearch, 700));
-refs.form.addEventListener('submit', e => e.preventDefault());
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  refs.input.blur();
+});
 
 function onSearch(e) {
   photosApiService.query = e.target.value;
@@ -17,9 +28,10 @@ function onSearch(e) {
 
   if (!photosApiService.searchQuery) return;
 
-  resetCardsMarckup();
-
-  photosApiService.fetchPhotos().then(renderCardsMarckup);
+  photosApiService.fetchPhotos()
+    .then(renderCardsMarckup)
+    .then(notifications)
+    .catch(onError);
 };
 
 observer.observe(refs.loadMore);

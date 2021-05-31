179a338716d064efa2c1bb5651615e2116d6364b
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-01 01:21:37 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 export default {
   gallery: document.querySelector('.gallery'),
   form: document.querySelector('#search-form'),
-  loadMore: document.querySelector('.check-point'),
+  loadMore: document.querySelector('.trigger'),
+  input: document.querySelector('.input'),
 }
